ea1b869a026201f02709750026281f98877afdaf
Suhrob <suxroberkinov438@gmail.com>
2026-05-11 13:42:20 +0500

    majburiy yuklash

--- node.js
+++ node.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.use(express.json());
 app.use(express.static(__dirname));
+
+const DATA_FILE = path.join(__dirname, 'data.json');
+
+app.get('/api/load', (req, res) => {
+    if (fs.existsSync(DATA_FILE)) {
+        const data = fs.readFileSync(DATA_FILE);
+        res.json(JSON.parse(data));
+    } else {
+        res.json({ participants: null, endTime: null });
+    }
+});
+
+app.post('/api/save', (req, res) => {
+    try {
+        fs.writeFileSync(DATA_FILE, JSON.stringify(req.body, null, 2));
+        res.send({ status: 'Ok', message: 'Saqlandi!' });
+    } catch (error) {
+        res.status(500).send({ status: 'Error', message: 'Saqlashda xato!' });
+    }
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Index.html'));
